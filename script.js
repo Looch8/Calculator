1c0d7289136539cc58e2baefdd36ff052aa741d3
Luke <lukewheldale@gmail.com>
2023-01-02 18:45:58 +1030

    added basic math operator functions and a function that takes an operator function, and 2 numbers and then performs the operation

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// functions for math operators
+const add = function (x, y) {
+  return x + y;
+};
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const multiply = function (x, y) {
+  return x * y;
+};
+
+const divide = function (x, y) {
+  return x / y;
+};
+
+// Function that takes an operator and 2 numbers and then calls one of the operator functions on the numbers
+function operate(operator, num1, num2) {
+  if (operator === `+`) {
+    return add(num1, num2);
+  } else if (operator === `-`) {
+    return subtract(num1, num2);
+  } else if (operator === `*`) {
+    return multiply(num1, num2);
+  } else if (operator === `/`) {
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate(`-`, 7, 3));
